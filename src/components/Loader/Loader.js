b3d980269e3011346d2756c352e4066979013c15
Matt Roden <mattroden92@gmail.com>
2021-12-04 14:52:38 -0700

    Import CircularProgress, no style changes  yet

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Loader.css'
+import CircularProgress from '@mui/material/CircularProgress';
 
 const Loader = () => {
   return (
     <div>
-      Loading!
+      <CircularProgress />
     </div>
   )
 }
