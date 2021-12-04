71e88a552e78e16177f09cd3b216220cb26cdab9
Matt Roden <mattroden92@gmail.com>
2021-12-04 15:04:20 -0700

    Remove pseudocode

--- src/components/AlbumSearchForm/AlbumSearchForm.js
+++ src/components/AlbumSearchForm/AlbumSearchForm.js
@@ -35,11 +35,3 @@ const AlbumSearchForm = ({ changeCurrentAlbumNumber }) => {
 }
 
 export default AlbumSearchForm
-
-//props: function for appending value to fetching function and updating state in app
-
-// state: 
-// - value - string/number
-
-// number input, value = state value, on change will set the state value
-// button, handleClick will send the input value into fetching method that will request photos from that number album
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -37,17 +37,3 @@ const App = () => {
 }
 
 export default App;
-
-// state: 
-// - currentAlbumNumber - starts out as null
-// - photos - starts out empty array
-// - isLoading - starts out false
-// - isError - starts out false
-
-// when the form passes back a number after submit,
-// app will append the number to the url being fetched in the utils file
-// the response will be set into photos state
-// photos will get passed down to PhotosContainer
-// Photos container will map through the photos prop and build each PhotoCard
-// every time that the form data changes (user picks a new number), 
-// All of the photos should update
