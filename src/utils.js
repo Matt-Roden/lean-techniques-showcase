7c1427e55ab6c29a58078034e68eed504c757d22
Matt Roden <mattroden92@gmail.com>
2021-12-04 15:45:48 -0700

    Add check for errors method

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,12 @@
 export const getPhotosByAlbum = (albumId) => {
   return fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`)
-  .then(response => response.json())
+  .then(response => checkForErrors(response))
+}
+
+const checkForErrors = (response) => {
+  if(!response.ok) {
+    throw new Error(`Oops, looks like something went wrong... Please try again.`)
+  } else {
+    return response.json()
+  }
 }
