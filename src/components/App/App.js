0983db2739fe49e7ac95148d05844cc2a48232c7
Matt Roden <mattroden92@gmail.com>
2021-12-04 13:24:31 -0700

    Add base skeleton to Error and Loader and import to App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Header from '../Header/Header';
 import AlbumSearchForm from '../AlbumSearchForm/AlbumSearchForm';
 import PhotosContainer from '../PhotosContainer/PhotosContainer';
+import Loader from '../Loader/Loader';
+import Error from '../Error/Error';
 
 const App = () => {
   return (
