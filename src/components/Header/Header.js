1e8f0f129bc276025e64f5da6be39c195925bb2f
Matt Roden <mattroden92@gmail.com>
2021-12-04 16:14:19 -0700

    Add structuring and container elements and import logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import './Header.css'
+import photoLogo from '../../images/photoLogo.png'
 
 const Header = () => {
   return (
     <header className='header'>
-      Header Here!
+      <div className='logo-and-text-container'>
+        <img 
+          className='logo'
+          src={photoLogo}
+          alt='app logo, polaroids'
+        />
+        <h2>My Photo Albums</h2>
+      </div>
     </header>
   )
 }
