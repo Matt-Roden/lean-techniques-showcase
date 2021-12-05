21c94b3976daa1b8a2b477a17d6bbc4aab985c38
Matt Roden <mattroden92@gmail.com>
2021-12-05 14:23:07 -0700

    Add form input and button styles

--- src/components/AlbumSearchForm/AlbumSearchForm.js
+++ src/components/AlbumSearchForm/AlbumSearchForm.js
@@ -15,18 +15,20 @@ const AlbumSearchForm = ({ changeCurrentAlbumNumber }) => {
   }
 
   return (
-    <div>
-      <h3>Select a Photo Album by Number (1-100)</h3>
-      <form>
+    <div className='form-container'>
+      <h3 className='form-message'>Select a Photo Album by Number (1-100)</h3>
+      <form className='form'>
         <input 
-          className='album-input'
+          className='form-input'
           type='number'
           value={value}
           onChange={(event) => setValue(event.target.value)}
           min='1'
           max='100'
+          placeholder='...'
         />
         <button
+          className='choose-button'
           onClick={(event) => handleClick(event)}
         >Choose Album</button>
       </form>
