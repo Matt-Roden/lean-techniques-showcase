3754f9074ebb5d0b45bae1109d4e4cb709a2bfab
Matt Roden <mattroden92@gmail.com>
2021-12-05 16:56:18 -0700

    Add fetchAlbumFour command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,29 @@ Cypress.Commands.add('fetchAlbumThree', () => {
   cy.get('form.form').find('input.form-input').type('3')
   cy.get('form.form').find('button.choose-button').click()
 }); // end command
+
+Cypress.Commands.add('fetchAlbumFour', () => {
+  cy.intercept('GET', 'https://jsonplaceholder.typicode.com/photos?albumId=4', {
+    statusCode: 200,
+    body: [
+      {
+        "albumId": 3,
+        "id": 401,
+        "title": "I'm from album 4",
+        "url": "https://via.placeholder.com/600/e743b",
+        "thumbnailUrl": "https://via.placeholder.com/150/e743b"
+      },
+      {
+        "albumId": 3,
+        "id": 402,
+        "title": "Yeah, I'm also from album 4",
+        "url": "https://via.placeholder.com/600/a393af",
+        "thumbnailUrl": "https://via.placeholder.com/150/a393af"
+      }
+    ]
+  })
+  cy.get('form.form').find('input.form-input').type('4')
+  cy.get('form.form').find('button.choose-button').click()
+}); // end command
+
+
