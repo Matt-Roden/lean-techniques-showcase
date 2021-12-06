e9c790aea70db21e6b417a0001e646e79e861dc4
Matt Roden <mattroden92@gmail.com>
2021-12-05 17:35:21 -0700

    Add fade in animations to card image and info text - smooth out fade in

--- src/components/PhotoCard/PhotoCard.js
+++ src/components/PhotoCard/PhotoCard.js
@@ -4,20 +4,27 @@ import { gsap } from 'gsap'
 
 const PhotoCard = ({ photoInfo }) => {
   let card = useRef(null);
+  let pic = useRef(null);
+  let infoContainer = useRef(null)
 
   useEffect(() => {
-    gsap.from([card], {
+    gsap.from([card, pic, infoContainer], {
       opacity: 0, 
-      duration: 1.1
+      duration: 1
     });
   }, [])
 
   return (
     <div className='full-card-container' ref={el => card = el }>
       <div className='photo-container'>
-        <img className='card-photo' src={photoInfo.url} alt='colorful placeholder'/>
+        <img 
+          className='card-photo' 
+          src={photoInfo.url} 
+          alt='colorful placeholder'
+          ref={el => pic = el }  
+        />
       </div>
-      <div className='photo-info-container'>
+      <div className='photo-info-container' ref={el => infoContainer = el }>
         <h5 className='photo-id-text card-text'>{`Photo ID: ${photoInfo.id}`}</h5>
         <p className='photo-title-text card-text'>{`Title: ${photoInfo.title}`}</p>
       </div>
