3f2feab4f828566e9cf7c24c661ec886ad89c046
Matt Roden <mattroden92@gmail.com>
2021-12-06 12:33:23 -0700

    Add commands and tests for id validation method

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -29,14 +29,14 @@ Cypress.Commands.add('fetchAlbumFour', () => {
     statusCode: 200,
     body: [
       {
-        "albumId": 3,
+        "albumId": 4,
         "id": 401,
         "title": "I'm from album 4",
         "url": "https://via.placeholder.com/600/e743b",
         "thumbnailUrl": "https://via.placeholder.com/150/e743b"
       },
       {
-        "albumId": 3,
+        "albumId": 4,
         "id": 402,
         "title": "Yeah, I'm also from album 4",
         "url": "https://via.placeholder.com/600/a393af",
@@ -48,4 +48,75 @@ Cypress.Commands.add('fetchAlbumFour', () => {
   cy.get('form.form').find('button.choose-button').click()
 }); // end command
 
+Cypress.Commands.add('enterTripleDigitsStartingWithTwoZeros', () => {
+  cy.intercept('GET', 'https://jsonplaceholder.typicode.com/photos?albumId=4', {
+    statusCode: 200,
+    body: [
+      {
+        "albumId": 4,
+        "id": 401,
+        "title": "I'm from album 4",
+        "url": "https://via.placeholder.com/600/e743b",
+        "thumbnailUrl": "https://via.placeholder.com/150/e743b"
+      },
+      {
+        "albumId": 4,
+        "id": 402,
+        "title": "Yeah, I'm also from album 4",
+        "url": "https://via.placeholder.com/600/a393af",
+        "thumbnailUrl": "https://via.placeholder.com/150/a393af"
+      }
+    ]
+  })
+  cy.get('form.form').find('input.form-input').type('004')
+  cy.get('form.form').find('button.choose-button').click()
+}); // end command
+
+Cypress.Commands.add('enterTripleDigitsStartingAndEndingWithZero', () => {
+  cy.intercept('GET', 'https://jsonplaceholder.typicode.com/photos?albumId=40', {
+    statusCode: 200,
+    body: [
+      {
+        "albumId": 4,
+        "id": 441,
+        "title": "I'm from album 40",
+        "url": "https://via.placeholder.com/600/e743b",
+        "thumbnailUrl": "https://via.placeholder.com/150/e743b"
+      },
+      {
+        "albumId": 4,
+        "id": 442,
+        "title": "Yeah, I'm also from album 40",
+        "url": "https://via.placeholder.com/600/a393af",
+        "thumbnailUrl": "https://via.placeholder.com/150/a393af"
+      }
+    ]
+  })
+  cy.get('form.form').find('input.form-input').type('040')
+  cy.get('form.form').find('button.choose-button').click()
+}); // end command
+
+Cypress.Commands.add('enterTripleDigitsStartingWithSingleZero', () => {
+  cy.intercept('GET', 'https://jsonplaceholder.typicode.com/photos?albumId=44', {
+    statusCode: 200,
+    body: [
+      {
+        "albumId": 4,
+        "id": 441,
+        "title": "I'm from album 44",
+        "url": "https://via.placeholder.com/600/e743b",
+        "thumbnailUrl": "https://via.placeholder.com/150/e743b"
+      },
+      {
+        "albumId": 4,
+        "id": 442,
+        "title": "Yeah, I'm also from album 44",
+        "url": "https://via.placeholder.com/600/a393af",
+        "thumbnailUrl": "https://via.placeholder.com/150/a393af"
+      }
+    ]
+  })
+  cy.get('form.form').find('input.form-input').type('044')
+  cy.get('form.form').find('button.choose-button').click()
+}); // end command
 
